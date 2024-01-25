407ca31028e0077d765c5bd42499f73cf4250885
ALI-ISHAQ <aliishaq3578@gmail.com>
2024-01-25 21:39:14 +0500

    #

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      
-      "https://trend-flare-apparel-store.vercel.app/",
+
+      "https://trend-flare-apparel-store.vercel.app",
       
     ],
     credentials: true,
